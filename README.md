a5dcfd2e9d522cf37f36d80ab9a24f320a2c16d5
a <ariiramanesh@gmail.com>
2021-06-25 18:45:13 -0700

    add poryscript and update emerald makefile

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+## Pokemon Gen 3
+This is aith's template install for Pokemon Gen 3 romhacks using pret's pokemon decompilations. Tested on Ubuntu 20.04.   
 
+This repo comes with:  
++ agbcc        
++ poryscript  (requires some manual changes in your chosen ROM's Makefile)  
+
+## Usage  
+Dependencies:  
++ Go Lang (for poryscript)
+
+To install everything else, use:  
+```  
+./get-all.sh [pokeemerald]  
+```  
--- get-all.sh
+++ get-all.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 ## For Ubuntu only
+set -x # display output
 
-## Get decompiled game
-git clone https://github.com/pret/pokeemerald
-
-## Get agbcc (compiler)
-git clone https://github.com/pret/agbcc
-cd agbcc
-./build.sh
-./install.sh ../pokeemerald
+GAME=${1:-pokeemerald}    
 
 ## Necessary packages
 sudo apt install build-essential binutils-arm-none-eabi git libpng-dev
 
-## Debug
+## Get decompiled game
+# git clone https://github.com/pret/pokeemerald
+
+## Get agbcc (compiler)
+# git clone https://github.com/pret/agbcc
+# agbcc/./build.sh
+# agbcc/./install.sh ./$GAME
+
+## Get Debug packages
 sudo apt install gdebi-core
+
+
+## Attach PoryScript to rom decomp
+# cp -r ./poryscript ./$GAME/tools/
+cd poryscript
+go build
+cp poryscript ../$GAME/tools/poryscript/
+cd ..
+poryscript/./install.sh ./$GAME
+
+### You will also need to follow the poryscript 
+### Makefile changes manually.
